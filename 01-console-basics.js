677a2580887de41ee9cc162b6f30a9a82a23c148
cem192 <cem192@miami.edu>
2018-09-27 15:06:40 -0400

    01 Almost complete
    Have a couple of questions

---
+++ 01-console-basics.js
@@ -0,0 +1,78 @@
+// Problem #1
+console.log("Hello");
+Hello
+console.log("Candice");
+Candice
+console.log("Mom's Cooking");
+ Mom's Cooking
+console.log('Mom's Cooking');
+//answer Uncaught syntax error: missing ) after argument list
+consoloe.log('Mom\'s cooking');
+Uncaught ReferenceError: consoloe is not defined at <anonymous>:1:1
+*****Ask Matt how to fix this
+
+// Problem #2
+document.write("Hello");
+Hello
+document.write("Goodbye");
+Goodbye
+document.write("Hack/Slash");
+Hack/Slash
+***** Ask about last task of this question
+
+// Problem #3
+window.alert("Hello");
+Pop up window says Hello
+window.confirm("Do you like JavaScript?");
+Pop up window asks Do you like JavaScript? with options ok or cancel. The value is True.
+window.prompt("What is your favorite programming language?");
+Pop up window asks What is your favorite programming language and includes text box for response. The value is my response, html.
+
+// Problem #4
+2+2;
+4
+2-2;
+0
+2*3;
+6
+6/3;
+2
+10%100;
+10
+
+//Problem #5
+Math.sqrt(25);
+5
+Math.PI;
+3.141592653589793
+Math.abs(-5);
+5. The 'abs' method changes negative numbers to positive numbers.
+Math.round(4.56);
+5. Makes numbers round up.
+Math.round(Math.PI);
+3
+Math.random()
+0.6126509272346439
+Math.random()
+0.5072832944032384
+Math.random()
+0.561216572252885. While the numbers produced are random, they are also always less than 0.
+
+//Problem #6
+"Hello".toUpperCase();
+"HELLO"
+"Hello".toLowerCase();
+"hello"
+"Hello".capitalize();
+reads as not a function
+**** ask matt about .capitalize
+
+//Problem #7
+"cat".replace("t", "r");
+"car"
+"sass".replace("s", "b");
+"bass"
+"   Hello   ".trim()
+"Hello" - cutes out extra spaces surrounding the letters contained within the quote marks
+"Hell, world!".length;
+12 - tells you how many characters the string contains
